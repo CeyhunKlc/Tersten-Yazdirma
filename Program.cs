5bcf86d2f9899ab448132034ba0b61369292092d
CeyhunKlc <117180182+CeyhunKlc@users.noreply.github.com>
2022-12-19 16:26:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace metniterscevirenuygulama
+{ 
+    class program
+    { 
+        static void Main(string[] args)
+        { 
+            string Metin;
+            Console.WriteLine("Lütfen Metin Giriniz:");
+            Metin = Console.ReadLine();
+            for (int i = 0; i < Metin.Length ; i++)
+            {
+                Console.Write(Metin[Metin.Length - i - 1]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
